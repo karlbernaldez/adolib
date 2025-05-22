ce7ff7542cbfd6bc353c5ce87d529b89fbe02027
Karl Santiago Bernaldez <bernaldezkarlsantiago@gmail.com>
2025-05-22 05:17:24 +0000

    fix(readme):logo link

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="adolib",
-    version="0.2.1",
+    version="0.2.2",
     description="A beginner-friendly Python utilities library",
     author="Karl Santiago Bernaldez",
     author_email="bernaldezkarlsantiago@gmail.com",
